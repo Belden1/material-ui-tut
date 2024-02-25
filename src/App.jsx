4832d37c1892bcc65ee157d5b5a43a0b40e14372
Kristopher Hellsten <carvajalhellstenmail@gmail.com>
2024-02-25 04:11:59 +0100

    Added routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import Notes from './pages/Notes';
 import Create from './pages/Create';
 import RootLayout from './layouts/RootLayout';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <Create />
+      <Routes>
+        <Route path="/" element={<Notes />} />
+        <Route path="/create" element={<Create />} />
+      </Routes>
     </div>
   );
 }
