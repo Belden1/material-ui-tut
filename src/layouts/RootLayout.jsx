6c38daff447416b7619b21f59bd239428169d054
Kristopher Hellsten <carvajalhellstenmail@gmail.com>
2024-02-26 10:13:48 +0100

    Lesson 13: Permanent Drawer

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -1,8 +1,18 @@
 import { Outlet, NavLink } from 'react-router-dom';
+import Drawer from '@mui/material/Drawer';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+
+const drawerWidth = 240;
 
 const RootLayout = () => {
   return (
-    <div className="root-layout">
+    <div
+      className="root-layout"
+      style={{
+        display: 'flex'
+      }}
+    >
       <header>
         <nav style={{ display: 'flex', gap: '10px' }}>
           <NavLink to="/">Home</NavLink>
@@ -14,6 +24,21 @@ const RootLayout = () => {
       {/* app bar */}
 
       {/* side drawer */}
+      <Drawer
+        variant="permanent"
+        anchor="left"
+        sx={{
+          width: drawerWidth,
+          '& .MuiDrawer-paper': {
+            width: drawerWidth,
+            boxSizing: 'border-box'
+          }
+        }}
+      >
+        <Box>
+          <Typography variant="h5">Ninja Notes</Typography>
+        </Box>
+      </Drawer>
 
       <main>
         <Outlet />
